ec1158642869a8ae844e90a1d345874633730e77
Myztiq <kahn@runnable.com>
2016-04-29 09:39:38 -0700

    Adding tests for canUseIntercom and upsertCompany

--- test/orion.js
+++ test/orion.js
@@ -7,9 +7,11 @@ const it = lab.test
 const sinon = require('sinon')
 const beforeEach = lab.beforeEach
 const afterEach = lab.afterEach
+const after = lab.after
 const expect = require('code').expect
 const Intercom = require('intercom-client')
 const mockIntercom = require('./mocks/mockIntercom')
+const proxyquire = require('proxyquire')
 
 const Orion = require('../lib/orion')
 
@@ -47,18 +49,116 @@ describe('orion', () => {
   })
 
   describe('canUseIntercom', () => {
-    const orion = new Orion()
+    const oldIntercomAPIKey = process.env.INTERCOM_API_KEY
+    const oldIntercomAppId = process.env.INTERCOM_APP_ID
+    let existsStub
+    let envIsStub
+    let orion
     beforeEach((done) => {
+      process.env.INTERCOM_API_KEY = 'fake api key'
+      process.env.INTERCOM_APP_ID = 'fake app id'
+      existsStub = sinon.stub().returns(false)
+      envIsStub = sinon.stub().returns(true)
+      const Orion = proxyquire('../lib/orion', {
+        '101/exists': existsStub,
+        '101/env-is': envIsStub
+      })
+      orion = new Orion()
+      existsStub.reset()
+      envIsStub.reset()
       done()
     })
 
-    it('should return false if no INTERCOM_APP_ID', function () {
-      delete process.env.INTERCOM_API_KEY
-      orion.canUseIntercom()
+    after((done) => {
+      process.env.INTERCOM_API_KEY = oldIntercomAPIKey
+      process.env.INTERCOM_APP_ID = oldIntercomAppId
+      done()
     })
-    it('should return false if no INTERCOM_APP_ID', function () {
+
+    it('should return false if no INTERCOM_APP_ID', (done) => {
+      delete process.env.INTERCOM_API_KEY
+      existsStub.withArgs(process.env.INTERCOM_API_KEY).returns(false)
+      existsStub.withArgs(process.env.INTERCOM_APP_ID).returns(true)
+      expect(orion.canUseIntercom()).to.equal(false)
+      sinon.assert.notCalled(envIsStub)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_API_KEY)
+      done()
+    })
+    it('should return false if no INTERCOM_APP_ID', (done) => {
       delete process.env.INTERCOM_APP_ID
-      orion.canUseIntercom()
+      existsStub.withArgs(process.env.INTERCOM_API_KEY).returns(true)
+      existsStub.withArgs(process.env.INTERCOM_APP_ID).returns(false)
+      expect(orion.canUseIntercom()).to.equal(false)
+      sinon.assert.notCalled(envIsStub)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_APP_ID)
+      done()
+    })
+    it('should return false if not the right env', (done) => {
+      existsStub.withArgs(process.env.INTERCOM_API_KEY).returns(true)
+      existsStub.withArgs(process.env.INTERCOM_APP_ID).returns(true)
+      expect(orion.canUseIntercom()).to.equal(false)
+      sinon.assert.calledOnce(envIsStub)
+      sinon.assert.calledWith(envIsStub, 'test')
+      sinon.assert.calledTwice(existsStub)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_API_KEY)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_APP_ID)
+      done()
+    })
+    it('should return true if everything is set properly', (done) => {
+      existsStub.withArgs(process.env.INTERCOM_API_KEY).returns(true)
+      existsStub.withArgs(process.env.INTERCOM_APP_ID).returns(true)
+      envIsStub.returns(false)
+      expect(orion.canUseIntercom()).to.equal(true)
+      sinon.assert.calledOnce(envIsStub)
+      sinon.assert.calledWith(envIsStub, 'test')
+      sinon.assert.calledTwice(existsStub)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_API_KEY)
+      sinon.assert.calledWith(existsStub, process.env.INTERCOM_APP_ID)
+      done()
+    })
+  })
+
+  describe('upsertCompany', () => {
+    const orion = new Orion()
+    const upsertParams = {
+      company_id: 'Company ID',
+      name: 'Company NAME!'
+    }
+    const resolved = Promise.resolve()
+    console.log('REALLY?', resolved.asCallback)
+    beforeEach((done) => {
+      sinon.stub(orion, 'canUseIntercom').returns(true)
+      orion.intercomClient = {
+        companies: {
+          create: sinon.stub().returns(resolved)
+        }
+      }
+      done()
+    })
+    afterEach((done) => {
+      orion.canUseIntercom.restore()
+      done()
+    })
+
+    it('should do nothing if we can\'t use intercom', (done) => {
+      orion.canUseIntercom.returns(false)
+      orion.upsertCompany(upsertParams)
+        .then(() => {
+          sinon.assert.calledOnce(orion.canUseIntercom)
+          sinon.assert.notCalled(orion.intercomClient.companies.create)
+        })
+        .asCallback(done)
+    })
+
+    it('should create the company if one exists', (done) => {
+      orion.canUseIntercom.returns(true)
+      orion.upsertCompany(upsertParams)
+        .then(() => {
+          sinon.assert.calledOnce(orion.canUseIntercom)
+          sinon.assert.calledOnce(orion.intercomClient.companies.create)
+          sinon.assert.calledWith(orion.intercomClient.companies.create, upsertParams)
+        })
+        .asCallback(done)
     })
   })
 })
